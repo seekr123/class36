b7c7f16b06e4bd413b6be7080a312a65a9e33ebf
seekr123 <aarnavchoudhary2006@gmail.com>
2020-12-12 18:37:04 +0530

    test

---
+++ js/form.js
@@ -0,0 +1,33 @@
+class Form{
+    constructor(){
+        this.title=createElement('h2'); 
+        this.input=createInput('name: ');
+        this.button=createButton('Play');
+        this.greeting=createElement('h3');
+    }
+    display(){
+          
+        this.title.html('Car Racing Game');
+        this.title.position(130,0);        
+        this.input.position(130,160);        
+        this.button.position(250,200);
+        
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+            player.name=this.input.value();
+            playerCount++;
+            player.index=playerCount;
+            player.update();
+            player.updateCount(playerCount);
+            this.greeting.html('hello  '+ player.name);
+            this.greeting.position(130,160);
+        })
+        
+     }
+     hide(){
+         this.input.hide();
+         this.button.hide();
+         this.greeting.hide();
+     }
+}
